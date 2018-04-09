32405a6b72161d76c10aa5782283554d665293f2
Eni Sinanaj <enisinanaj@outlook.com>
2018-04-09 23:34:04 +0200

    working node

---
+++ ios/Pods/Target Support Files/Pods-v1rgo/Pods-v1rgo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_v1rgo : NSObject
+@end
+@implementation PodsDummy_Pods_v1rgo
+@end
---
+++ ios/Pods/Target Support Files/Pods-v1rgoTests/Pods-v1rgoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_v1rgoTests : NSObject
+@end
+@implementation PodsDummy_Pods_v1rgoTests
+@end

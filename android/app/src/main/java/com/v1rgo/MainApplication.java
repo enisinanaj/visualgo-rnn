cd1c83617ba9a1c6ce5d6743adfe83d1aebf7ed8
Eni Sinanaj <enisinanaj@outlook.com>
2018-04-13 00:55:59 +0200

    camera

--- android/app/src/main/java/com/v1rgo/MainApplication.java
+++ android/app/src/main/java/com/v1rgo/MainApplication.java
@@ -3,6 +3,7 @@ package com.v1rgo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.benwixen.rnfilesystem.RNFileSystemPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new VectorIconsPackage(),
             new RNFileSystemPackage(),
             new RNFetchBlobPackage(),
